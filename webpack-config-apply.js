409b8a073be4cf2019d56722a067048fc24b6582
russa <aaron.russ@dfki.de>
2018-11-27 18:30:19 +0100

    redirect entry-point of mmir-lib to webpack bootstrapping file & code cleanup

--- webpack-config-apply.js
+++ webpack-config-apply.js
@@ -1,34 +1,20 @@
 const path = require('path');
-// const webpack = require('webpack');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var fileUtils = require('./webpack-filepath-utils.js');
 var appConfigUtils = require('./webpack-app-module-config-utils.js');
 
-// ---------------------------------------
 
-// var AddToContextPlugin = require('./webpack-plugin-add-to-context.js');
 var ReplaceModuleIdPlugin = require('./webpack-plugin-replace-id.js');
-
-// var VirtualModulePlugin = require('virtual-module-webpack-plugin');
-
-// ---------------------------------------
 
 var isDisableLogging = false;
 
 // console.log('mmir-lib: ', require('mmir-lib'))
 
-//FIXME
-// var rootDir = __dirname;
-var rootDir;
-// try {
-	rootDir = path.dirname(require.resolve('mmir-lib'));
-// } catch(err){
-// 	rootDir = __dirname;
-// }
+var rootDir = path.dirname(require.resolve('mmir-lib'));
 var webpackRootDir = __dirname;
-console.log('mmir-lib path: ', rootDir);
-console.log('mmir-webpack path: ', webpackRootDir);
+
+// console.log('mmir-lib path: ', rootDir);
+// console.log('mmir-webpack path: ', webpackRootDir);
 
 // console.log('######### cwd: ', process.cwd());
 // console.log('######### argv[0]: ', process.argv[0]);
@@ -40,28 +26,14 @@ var createResolveAlias = function(mmirAppConfig){
 	var paths = require('./webpack-resources-paths.js').paths;
 
 	var alias = {};
-	// var aliasList = [];
 	for (var n in paths) {
 		if(/^build-tool\//.test(n)){
 			alias[n] = path.join(webpackRootDir, paths[n]);
 		} else {
 			alias[n] = path.join(rootDir, paths[n]);
 		}
-		// aliasList.push(n);
 	}
 
-	// if(mmirAppConfig && mmirAppConfig.paths){
-	// 	// console.log('adding/overwriting paths with app paths: ', mmirAppConfig.paths);
-	// 	// Object.assign(alias, mmirAppConfig.paths);
-	// 	var appRoot = mmirAppConfig.rootPath || process.cwd();
-	// 	var p;
-	// 	for (var n in mmirAppConfig.paths) {
-	// 		p = mmirAppConfig.paths[n];
-	// 		alias[n] = path.isAbsolute(p)? p : path.join(appRoot, p);
-	// 		// aliasList.push(n);
-	// 	}
-	// 	// console.log('set paths to -> ', alias);
-	// }
 	appConfigUtils.addAliasFrom(mmirAppConfig, alias);
 
 	if(isDisableLogging){
@@ -73,13 +45,6 @@ var createResolveAlias = function(mmirAppConfig){
 
 var createModuleRules = function(mmirAppConfig){
 
-
-	// var mmirAppConfigContent = 'module.exports=';
-	// if(mmirAppConfig){
-	// 	mmirAppConfigContent = JSON.stringify({config: mmirAppConfig.config});
-	// } else {
-	// 	mmirAppConfigContent = 'false';
-	// }
 	var mmirAppConfigContent = appConfigUtils.generateModuleCode(mmirAppConfig);
 
 	// console.log('###### creating module webpack-app-module-config.js with contents: '+ JSON.stringify(mmirAppConfigContent));
@@ -94,61 +59,16 @@ var createModuleRules = function(mmirAppConfig){
 
 	var appConfigModulePath = fileUtils.normalizePath(path.join(webpackRootDir, 'webpack-app-module-config.js'));
 
-	//convert shims
-	var shims = [
-		//Shim Examples
-		// {
-		//   test: /handlebars/,
-		//   use: [
-		//     'imports-loader?this=>window',
-		//     'exports-loader?Handlebars'
-		//   ]
-		// },
-		// {
-		//   test: /jquery\-1\.9\.1\.js/,
-		//   use: [
-		//     'imports-loader?this=>window',
-		//     'exports-loader?jQuery'
-		//   ]
-		// },
-		// {
-		//   test: /backbone/,
-		//   use: [
-		//     'expose-loader?Backbone',
-		//     'imports-loader?this=>window,jquery,underscore'
-		//   ]
-		// },
-		// {
-		//   test: /underscore/,
-		//   loader: 'expose-loader?_' },
-		// {
-		//   test: /chaplin/,
-		//   loader: 'imports-loader?this=>window,backbone'
-		// },
+	var moduleRules = [
 
-
-		// {
-		//   test: /main\.js/,
-		//   use: [
-		//     'imports-loader?webpackMmirConfig=build-tool/webpack-helper-module-config,logger=mmirf/logger',
-		//     'exports-loader?window.mmir'
-		//   ]
-		// },
-		// {
-		//   test: /core\.js/,
-		//   use: [
-		//     'imports-loader?moduleConfigHelper=build-tool/module-config-helper',
-		//     'exports-loader?window.mmir'
-		//   ]
-		// },
-
+		// handle binary files (e.g. *.mp3):
 		{
 			test: fileUtils.createFileTestFunc(binFilePaths, ' for [bin file]'),
 			use: {
 				loader: 'file-loader',
 				options: {
-					// name: '[path][name].[ext]',
 					name: function(file) {
+						//use relative path path (from root) in target/output directory for the resouce:
 						if(file.indexOf(rootDir) === 0){
 							file = file.substring(rootDir.length).replace(/^(\\|\/)/, '');
 						}
@@ -158,6 +78,8 @@ var createModuleRules = function(mmirAppConfig){
 				}
 			}
 		},
+
+		// creates a module implementation for the app-config:
 		{
 			test: fileUtils.createFileTestFunc([appConfigModulePath], ' for [app config]'),
 			use: {
@@ -167,6 +89,8 @@ var createModuleRules = function(mmirAppConfig){
         }
 			}
 		},
+
+		// handle/include "raw" text files:
 		{
 			test: fileUtils.createFileTestFunc(textFilePaths, ' for [text files]'),
 			use: {
@@ -175,42 +99,47 @@ var createModuleRules = function(mmirAppConfig){
 		}
 
 	];
-	return shims;
+	return moduleRules;
 }
 
-var createPlugins = function(webpackInstance, alias){//, mmirAppConfig){
-	//
-	// var mmirAppConfigContent = "/*virtual module for injecting mmir app-specific configuration*/\nmodule.exports=";
-	// if(mmirAppConfig){
-	// 	mmirAppConfigContent += (typeof mmirAppConfig === 'string'? mmirAppConfig : JSON.stringify({config: mmirAppConfig.config}));
-	// } else {
-	// 	mmirAppConfigContent += "false;"
-	// }
-	//
-	// console.log('###### creating module webpack-app-module-config.js with contents: '+ JSON.stringify(mmirAppConfigContent));
+var createPlugins = function(webpackInstance, alias){
 
 	var plugins = [
+
+		// enable replacement implementation for requirejs' module.config()
+		// NOTE: in difference to the requirejs implementation, this does need the module as first parameter, i.e. something like
+		//      module.config(module) -> webpack_build_tool_module_config_helper_config(module)
 		new webpackInstance.ProvidePlugin({
 			'module.config': ['build-tool/module-config-helper', 'config'],
 		}),
+
+		// ignore modules that are specific for running mmir in node environment:
 		new webpackInstance.IgnorePlugin(/^xmlhttprequest$/),
-		// new AddToContextPlugin(aliasList),
+
+		// set custom module-IDs from alias-definitions for mmir-modules (enables mmir.require(<moduleId>))
 		new ReplaceModuleIdPlugin(alias, rootDir),
-		// new CopyWebpackPlugin([
-		// 	// { from: 'workers/*', to: 'mmirf' },
-		// 	{
-		// 		from: 'vendor/sounds/*',
-		// 		to: 'mmirf'
-		// 	},
-		// ]) //, options)
-		// new VirtualModulePlugin({
-		// 	moduleName: './webpack-app-module-config.js',
-		// 	contents: mmirAppConfigContent
-		// }),
+
+		// set environment variable WEBPACK_BUILD for enabling webpack-specific code
 		new webpackInstance.DefinePlugin({
 			'WEBPACK_BUILD': JSON.stringify(true),
-		})
+		}),
+
+		// redirect entry-point of mmir-lib to the webpack bootstrap/entry point
+		new webpackInstance.NormalModuleReplacementPlugin(
+			/requirejs-main\.js/,
+			function(resource) {
+				if(resource.rawRequest === 'mmir-lib'){//<- only redirect, if the file was requested via 'mmir-lib' (e.g. if requested directly via its path, do not redirect)
+					// console.log('replacing module ', resource.request, ' -> ', path.join(webpackRootDir, 'webpack-main.js'))//, ', for ', resource);
+					resource.request = path.join(webpackRootDir, 'webpack-main.js');
+					resource.resource = resource.request;
+				}
+				// else {
+				// 	console.log('ingnoring module replacement for ', resource.request);
+				// }
+			}
+		)
 	];
+
 	return plugins;
 };
 
@@ -250,21 +179,8 @@ module.exports = {
 			}
 		}
 
-		// // add loader resolving:
-		// //  * worker-loader
-		// //  * file-loader
-		// if(!webpackConfig.resolveLoader){
-		// 	webpackConfig.resolveLoader = {moduleExtensions: ["-loader"]};
-		// } else {
-		// 	if(webpackConfig.resolveLoader.moduleExtensions){
-		// 		webpackConfig.resolveLoader.moduleExtensions.push("-loader");
-		// 	} else {
-		// 		webpackConfig.resolveLoader.moduleExtensions = ["-loader"];
-		// 	}
-		// }
-
 		//add loader configurations:
-		var shims = createModuleRules(mmirAppConfig);
+		var moduleRules = createModuleRules(mmirAppConfig);
 		if(!webpackConfig.module){
 			webpackConfig.module = {};
 		}
@@ -273,8 +189,8 @@ module.exports = {
 			targetList = [];
 			webpackConfig.module[useRulesForLoaders? 'rules': 'loaders'] = targetList;
 		}
-		for(var i = 0, size = shims.length; i < size; ++i){
-			targetList.push(shims[i]);
+		for(var i = 0, size = moduleRules.length; i < size; ++i){
+			targetList.push(moduleRules[i]);
 		}
 
 		//add plugins
