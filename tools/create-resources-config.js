2fe3ca4a75f3b13b21f12b5307f7c26caa3970d1
russa <aaron.russ@dfki.de>
2019-07-29 13:16:26 +0200

    use new directory structure of wp_project_structure branch

--- tools/create-resources-config.js
+++ tools/create-resources-config.js
@@ -1,7 +1,7 @@
 
 var _ = require('lodash');
 
-var mmirModuleBaseConfig = _.cloneDeep(require('mmir-lib/modulesBaseConfig'));
+var mmirModuleBaseConfig = _.cloneDeep(require('mmir-lib/src/modulesBaseConfig'));
 
 // //remove paths that match the ID from mmir base paths:
 // var removePathsList = ['jquery'];
