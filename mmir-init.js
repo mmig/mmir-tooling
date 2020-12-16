43b1cc098b9e0c04916bd2b54ae43aeadf46e076
russa <aaron.russ@dfki.de>
2020-12-17 00:33:39 +0100

    recompiled sources

--- mmir-init.js
+++ mmir-init.js
@@ -1,14 +1,33 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
-const mmir_lib_1 = __importDefault(require("mmir-lib"));
+const mmirLib = __importStar(require("mmir-lib"));
 // log('########################## mmir-loader: set start module ', mmirLib.mmir.startModule);//, ', config-paths: ', mmirLib.config.paths);
-var mmir = mmir_lib_1.default.init(function (mmir) {
+var mmir = mmirLib.init(function (mmir) {
     mmir.startModule = 'mmirf/main-minimal';
-    (mmir_lib_1.default._config || mmir_lib_1.default.config).paths['mmirf/main-minimal'] = path_1.default.join(__dirname, 'main-minimal');
+    (mmirLib._config || mmirLib.config).paths['mmirf/main-minimal'] = path_1.default.join(__dirname, 'main-minimal');
     mmir.config({
         config: {
             'mmirf/configurationManager': {
