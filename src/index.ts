830eaac430f13458e4e959e40a52e8abcfdd55f2
russa <aaron.russ@dfki.de>
2021-06-10 18:25:31 +0200

    NOOP (removed disabled code)

--- src/index.ts
+++ src/index.ts
@@ -160,67 +160,6 @@ function compileResources(mmirAppConfig: BuildAppConfig): Promise<Array<Error|Er
         }).then(function(res){console.log('  completed processing scxml files.'); return res;}));
     }
 
-    //TODO? impl. processing?
-    // if(buildConfig.implList.length > 0){
-    // 	// load & pre-process implemetation files if necessary
-    // 	moduleRules.push({
-    // 		test: fileUtils.createFileTestFunc(buildConfig.implList.map(function(s){return s.file;}), ' for [controller | helper | model] files'),
-    // 		use: {
-    // 			loader: path.resolve(toolingRootDir, 'impl', 'impl-loader.js'),
-    // 			options: {mapping: buildConfig.implList},
-    // 		},
-    // 		type: 'javascript/auto'
-    // 	});
-    // }
-
-    //TODO? settings processing?
-    // var settingsFiles = buildConfig.settings.filter(function(s){return s.include === 'file';});
-    // if(settingsFiles && settingsFiles.length > 0){
-    //
-    // 	if(!mmirAppConfig.webpackPlugins){
-    // 		mmirAppConfig.webpackPlugins = [];
-    // 	}
-    //
-    // 	mmirAppConfig.webpackPlugins.push(function(webpackInstance, alias){
-    // 		return new webpackInstance.NormalModuleReplacementPlugin(
-    // 			/mmirf\/settings\/(configuration|dictionary|grammar|speech)\//i,
-    // 			function(resource) {
-    // 				if(alias[resource.request]){
-    //
-    // 					// console.log('NormalModuleReplacementPlugin: redirecting resource: ', resource, ' -> ', alias[resource.request]);//DEBU
-    //
-    // 					// resource.request = alias[resource.request];//DISABLED hard-rewire request ... instead, add an alias resolver (see below)
-    //
-    // 					var ca = {};
-    // 					ca[resource.request] = alias[resource.request];
-    // 					resource.resolveOptions = {alias: ca};
-    // 				}
-    // 			}
-    // 	)});
-    //
-    // 	moduleRules.push({
-    // 		test: fileUtils.createFileTestFunc(buildConfig.settings.filter(function(s){return !_.isArray(s.file) && s.type !== 'grammar';}).map(function(s){return s.file;}), ' for [language resource] files'),
-    // 		use: {
-    // 			loader: 'file-loader',
-    // 			options: {
-    // 				name: function(file) {
-    //
-    // 					// var ofile = file;//DEBU
-    //
-    // 					var root = appRootDir;
-    // 					//use relative path (from root) in target/output directory for the resouce:
-    // 					if(file.indexOf(root) === 0){
-    // 						file = file.substring(root.length).replace(/^(\\|\/)/, '');
-    // 					}
-    // 					// console.log('including [from '+root+'] dictionary file ', ofile , ' -> ', file)//DEBU
-    // 					return file;
-    // 				}
-    // 			}
-    // 		},
-    // 		type: 'javascript/auto'
-    // 	});
-    // }
-
     return tasks;
 }
 
